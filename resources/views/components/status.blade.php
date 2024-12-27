e7c2d4281872d63d576b0533eb0fd600aaf3c28d
Nadim Salloum <github@salloum.pm>
2024-12-27 21:03:13 +0100

    fix pill layout on mobile

--- resources/views/components/status.blade.php
+++ resources/views/components/status.blade.php
@@ -1,17 +1,17 @@
 @if($status == 'signed_up')
-    <span class="flex items-center justify-center sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-blue-500 bg-blue-100 rounded-full">
+    <span class="flex items-center justify-center w-28 sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-blue-500 bg-blue-100 rounded-full">
         eingetragen
     </span>
 @elseif($status == 'registered')
-    <span class="flex items-center justify-center sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-green-500 bg-green-100 rounded-full">
+    <span class="flex items-center justify-center w-28 sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-green-500 bg-green-100 rounded-full">
         angemeldet
     </span>
 @elseif($status == 'attended')
-    <span class="flex items-center justify-center sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-purple-400 bg-purple-100 rounded-full">
+    <span class="flex items-center justify-center w-28 sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-purple-400 bg-purple-100 rounded-full">
         teilgenommen
     </span>
 @elseif($status == 'cancelled')
-    <span class="flex items-center justify-center sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-red-500 bg-red-100 rounded-full">
+    <span class="flex items-center justify-center w-28 sm:w-32 h-6 sm:h-8 text-xs uppercase font-bold text-red-500 bg-red-100 rounded-full">
         abgesagt
     </span>
 @endif
