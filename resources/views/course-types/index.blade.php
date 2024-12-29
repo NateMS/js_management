fef2b86843d91d1d32a823e7fca3aff2a4a50ac3
Nadim Salloum <github@salloum.pm>
2024-12-29 22:23:49 +0100

    small text fixes

--- resources/views/course-types/index.blade.php
+++ resources/views/course-types/index.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
     <x-content-view>  
         <div class="flex justify-end">
-            <a href="{{ route('course-types.create') }}" class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Neuen Kurstyp erstellen</a>
+            <a href="{{ route('course-types.create') }}" class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Neuer Kurstyp</a>
         </div>
         @if($courseTypes->isEmpty())
             <x-no-data>Keine Kurstypen erfasst.</x-no-data>
--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -20,7 +20,7 @@
             </span>
             <div>
                 <span class="flex justify-end">
-                    <a href="{{ route('courses.create') }}" class="align-middle px-4 py-2 bg-gray-800 border border-transparent rounded-md inline-block font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Neuen Kurs erfassen</a>
+                    <a href="{{ route('courses.create') }}" class="align-middle px-4 py-2 bg-gray-800 border border-transparent rounded-md inline-block font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Neuer Kurs</a>
                 </span>    
             </div>
         </div>
